7b46ea4eb993b5e22ef718df75c502cdf892d356
Darren <gitdarrenchee@gmail.com>
2024-09-14 18:11:54 +0800

    connected FE to BE via websockets

--- sgtradex-frontend/sgtradex-fe/src/App.tsx
+++ sgtradex-frontend/sgtradex-fe/src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import io from 'socket.io-client';
 import VesselTable from './components/VesselTable';
 
+const socket = io("http://localhost:5000");
+
 function App() {
+
+  function test() {
+    socket.emit("send_message", { test: 'test' });
+  }
+
+  React.useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.test)
+    });
+  }, [socket]);
+
   return (
     <div className="center">
+      <button onClick={test}>click</button>
       <VesselTable />
     </div>
   );
--- sgtradex-server/server.js
+++ sgtradex-server/server.js
@@ -1,3 +1,29 @@
 const express = require('express')
-const WebSocket = require('ws')
 const http = require('http')
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ["POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit("receive_message", data)
+    })
+});
+
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
